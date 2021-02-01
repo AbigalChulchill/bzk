744ac41ffb8be2fe44a4f0521e7fdf009132a3a0
kirin <kirin.chen1001@gmail.com>
2021-02-01 23:41:00 +0800

    add saved-flow-client.service.ts

--- flow-editor/src/app/model/saved-flow.ts
+++ flow-editor/src/app/model/saved-flow.ts
@@ -1,2 +1,9 @@
+import { Flow } from './flow';
 export class SavedFlow {
+
+  public uid:string;
+  public model:Flow;
+  public updateAt:Date;
+  public createAt:Date;
+
 }
--- flow-editor/src/app/service/saved-flow-client.service.ts
+++ flow-editor/src/app/service/saved-flow-client.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { SavedFlow } from '../model/saved-flow';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Flow } from '../model/flow';
 @Injectable({
   providedIn: 'root'
 })
 export class SavedFlowClientService {
 
-  constructor() { }
+  public static URL_PREFIX = 'saved/flow/';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public listAll(): Observable<Array<SavedFlow>> {
+    return this.httpClient.get<Array<SavedFlow>>(environment.apiHost + SavedFlowClientService.URL_PREFIX + 'all');
+  }
+
+  public getByUid(uid: string): Observable<SavedFlow> {
+    return this.httpClient.get<SavedFlow>(environment.apiHost + SavedFlowClientService.URL_PREFIX + uid);
+  }
+
+  public remove(uid: string): Observable<void> {
+    return this.httpClient.delete<void>(environment.apiHost + SavedFlowClientService.URL_PREFIX + uid);
+  }
+
+  public save(m: Flow): Observable<SavedFlow> {
+    return this.httpClient.post<SavedFlow>(environment.apiHost + SavedFlowClientService.URL_PREFIX, m);
+  }
+
+
 }
