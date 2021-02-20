7f8575dc3276cbafd9b3f6e1c290c308a7379d76
Kirin <a29243004@gmail.com>
2021-02-20 14:07:27 +0800

    done the list saved flow

--- flow-editor/src/app/registered-flow/registered-flow.component.ts
+++ flow-editor/src/app/registered-flow/registered-flow.component.ts
@@ -6,18 +6,30 @@ import { SavedFlowClientService } from './../service/saved-flow-client.service';
 import { FlowClientService } from './../service/flow-client.service';
 import { LoadingService } from './../service/loading.service';
 import { FlowPoolInfo, FlowState } from './../dto/flow-pool-info';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClientService } from '../service/http-client.service';
 import { SavedFlow } from '../model/saved-flow';
 import { LoadSource, ModifyingFlowService } from '../service/modifying-flow.service';
 import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
+
 
 @Component({
   selector: 'app-registered-flow',
   templateUrl: './registered-flow.component.html',
   styleUrls: ['./registered-flow.component.css']
 })
-export class RegisteredFlowComponent implements OnInit {
+export class RegisteredFlowComponent implements OnInit,AfterViewInit {
+
+  displayedColumns: string[] = ['id', 'name', 'lastState', 'lastTriggerAt', 'triggerInfo', 'actions'];
+  dataSource: MatTableDataSource<Row>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   public FlowState = FlowState;
   public flowPoolInfos = new Array<FlowPoolInfo>();
   public savedFlows: Array<SavedFlow>;
@@ -30,17 +42,39 @@ export class RegisteredFlowComponent implements OnInit {
     private router: Router,
     private githubService: GithubService,
     private dialog: DialogService
-  ) { }
+  ) {
+    this.dataSource = new MatTableDataSource(new Array<Row>());
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
   async ngOnInit(): Promise<void> {
     await this.reflesh();
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
   private async reflesh(): Promise<void> {
     const t = this.loading.show();
     this.savedFlows = await this.savedFlowClient.listAll();
-    this.loading.dismiss(t);
     this.flowPoolInfos = await this.flowClient.listFlowPoolInfo().toPromise();
+    const vdata = new Array<Row>();
+    for (const sf of this.savedFlows) {
+      vdata.push(this.genRow(sf));
+    }
+    this.dataSource.data = vdata;
+    this.loading.dismiss(t);
+
   }
 
   public getPoolInfo(sf: SavedFlow): FlowPoolInfo {
@@ -71,7 +105,7 @@ export class RegisteredFlowComponent implements OnInit {
   }
 
   public async uploadAllGistDialog(): Promise<void> {
-    const t= this.loading.show();
+    const t = this.loading.show();
     const ml = new Array<Flow>();
     for (const sf of this.savedFlows) {
       ml.push(sf.model);
@@ -80,7 +114,8 @@ export class RegisteredFlowComponent implements OnInit {
     this.loading.dismiss(t);
   }
 
-  public async onFileEdit(gr: SavedFlow): Promise<void> {
+  public async onFileEdit(grid: string): Promise<void> {
+    const gr = this.getSavedFlow(grid);
     this.modifyingFlow.setTarget(gr.model, {
       id: gr.uid,
       source: LoadSource.terminal
@@ -88,4 +123,24 @@ export class RegisteredFlowComponent implements OnInit {
     this.router.navigate(['model/design']);
   }
 
+  public getSavedFlow(grid:string):SavedFlow { return this.savedFlows.find(f=> f.uid === grid); }
+
+  private genRow(sf: SavedFlow): Row {
+    const ans = new Row();
+    ans.id = sf.uid;
+    ans.name = sf.model.name;
+    const fr = this.getPoolInfo(sf);
+    if (!fr || fr.runInfos.length <= 0) return ans;
+    const lri = fr.runInfos[fr.runInfos.length - 1];
+    ans.lastState = lri.state;
+    return ans;
+  }
+
+}
+
+export class Row {
+  public id: string;
+  public name: string;
+  public lastState: string;
+  public lastTriggerAt: string;
 }
