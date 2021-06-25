e4ccbdbae1639513365ff6599a3556147fa6a816
kirin <kirin.chen1001@gmail.com>
2021-06-25 23:15:17 +0800

    fix the null

--- bzk/bzkflow/src/main/java/net/bzk/flow/run/action/SubFlowActionCall.java
+++ bzk/bzkflow/src/main/java/net/bzk/flow/run/action/SubFlowActionCall.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ExecutorService;
 
 import javax.inject.Inject;
 
+import net.bzk.flow.run.flow.FlowRuner;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
@@ -26,54 +28,60 @@ import net.bzk.infrastructure.ex.BzkRuntimeException;
 @Slf4j
 public class SubFlowActionCall extends ActionCall<SubFlowAction> {
 
-	@Inject
-	@Qualifier("threadPoolTaskExecutor")
-	private ExecutorService executorService;
+    @Inject
+    @Qualifier("threadPoolTaskExecutor")
+    private ExecutorService executorService;
 
-	@Inject
-	private RunFlowPoolDao flowPoolDao;
+    @Inject
+    private RunFlowPoolDao flowPoolDao;
 
-	public SubFlowActionCall() {
-		super(SubFlowAction.class);
-	}
+    public SubFlowActionCall() {
+        super(SubFlowAction.class);
+    }
 
-	@Override
-	public VarValSet call() throws Exception {
+    @Override
+    public VarValSet call() throws Exception {
 
-		var frp = flowPoolDao.getPool(getModel().getFlowUid());
-		var fr = frp.create();
+        var frp = flowPoolDao.getPool(getModel().getFlowUid());
+        var fr = frp.create();
 
-		List<KVPair> kvs = getModel().getInputData();
-		for (var kp : kvs) {
-			Object rv = getPolyglotEngine().parseScriptbleText(kp.getVal(), Object.class);
-			var kinfo = VarLv.checkLvByPrefix(kp.getKey());
-			fr.getVars().put(kinfo.getKey(), rv);
-		}
-		if (getModel().isAsynced()) {
-			executorService.execute(fr::run);
-			return new VarValSet();
-		}
+        List<KVPair> kvs = getModel().getInputData();
+        for (var kp : kvs) {
+            Object rv = getPolyglotEngine().parseScriptbleText(kp.getVal(), Object.class);
+            var kinfo = VarLv.checkLvByPrefix(kp.getKey());
+            fr.getVars().put(kinfo.getKey(), rv);
+        }
+        if (getModel().isAsynced()) {
+            executorService.execute(fr::run);
+            return new VarValSet();
+        }
 
-		fr.run();
-		if(fr.getInfo().getState() == State.Fail) {
-			logUtils.logActionCall(getUids(), "getState is Fail");
-			throw new BzkRuntimeException(fr.getInfo().getTransition().getEndTag());
-		}
-		List<VarVal> ers = fr.getInfo().getEndResult();
-		VarValSet ans = new VarValSet();
-		List<VarKeyReflect> omap = getModel().getOutputReflects();
-		for (VarVal vv : ers) {
-			Optional<VarKeyReflect> vkro = VarKeyReflect.findBySrcKey(omap, vv.getKey());
-			if (vkro.isEmpty())
-				continue;
-			VarKeyReflect vkr = vkro.get();
-			VarVal nvv = new VarVal();
-			nvv.setKey(vkr.getToKey().getKey());
-			nvv.setLv(vkr.getToKey().getLv());
-			nvv.setVal(vv.getVal());
-			ans.add(nvv);
-		}
-		return ans;
-	}
+        fr.run();
+        if (fr.getInfo().getState() == State.Fail) {
+            logUtils.logActionCall(getUids(), "getState is Fail");
+            throw new BzkRuntimeException(getFailEndTag(fr.getInfo()));
+        }
+        List<VarVal> ers = fr.getInfo().getEndResult();
+        VarValSet ans = new VarValSet();
+        List<VarKeyReflect> omap = getModel().getOutputReflects();
+        for (VarVal vv : ers) {
+            Optional<VarKeyReflect> vkro = VarKeyReflect.findBySrcKey(omap, vv.getKey());
+            if (vkro.isEmpty())
+                continue;
+            VarKeyReflect vkr = vkro.get();
+            VarVal nvv = new VarVal();
+            nvv.setKey(vkr.getToKey().getKey());
+            nvv.setLv(vkr.getToKey().getLv());
+            nvv.setVal(vv.getVal());
+            ans.add(nvv);
+        }
+        return ans;
+    }
+
+    private String getFailEndTag(FlowRuner.RunInfo ri) {
+        if (ri.getTransition() == null) return StringUtils.EMPTY;
+        if (ri.getTransition().getEndTag() == null) return StringUtils.EMPTY;
+        return ri.getTransition().getEndTag();
+    }
 
 }
