f50c4295293b138c58a9d2c98401860f1dec806e
kirin <kirin.chen1001@gmail.com>
2021-06-27 21:15:52 +0800

    add Serializable in VarVal.java

--- bzk/bzkflow/src/main/java/net/bzk/flow/model/var/VarVal.java
+++ bzk/bzkflow/src/main/java/net/bzk/flow/model/var/VarVal.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
-public class VarVal {
+public class VarVal implements Serializable {
 
 	private VarLv lv;
 	private String key;
