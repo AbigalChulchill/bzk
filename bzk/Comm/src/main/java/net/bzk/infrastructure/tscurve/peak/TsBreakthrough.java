cd23fe84dc129a8cb71f4bbd0b8407508bcf2aa8
kirin <kirin.chen1985@gmail.com>
2021-10-14 22:16:14 +0800

    TODO TsBreakthrough

---
+++ bzk/Comm/src/main/java/net/bzk/infrastructure/tscurve/peak/TsBreakthrough.java
@@ -0,0 +1,37 @@
+package net.bzk.infrastructure.tscurve.peak;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.beanutils.PropertyUtilsBean;
+
+public class TsBreakthrough {
+
+
+
+
+    public TsBreakthrough(){
+    }
+
+    public Result calc(TsPeakFinder.TrendInfo info){
+
+    }
+
+    public static enum Kind{
+        BREAKTHROUGH,HOLD_ON
+    }
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Result{
+
+        private Kind kind;
+        private boolean isCurrentBreaking;
+
+    }
+
+
+
+}
